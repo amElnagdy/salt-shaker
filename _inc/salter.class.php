29470cec2f7aaedda9179d151624a1ebb7a8619e
amElnagdy <ahmed.m@icanlocalize.com>
2016-10-08 15:03:43 +0200

    No-static methods should not be called statically

--- _inc/salter.class.php
+++ _inc/salter.class.php
@@ -12,23 +12,23 @@ class Salter extends SalterCore{
         add_action("salt_shaker_change_salts", array(&$this, "shuffleSalts"));
     }
     
-    public function add_menu_item(){
+    public static function add_menu_item(){
         add_submenu_page( "tools.php",  __("Salt Shaker Settings", SALT_SHAKER_DOMAIN), __("Salt Shaker", SALT_SHAKER_DOMAIN), "manage_options", "salt_shaker", array(__CLASS__, "admin_page_content") );
     }
     
-    public function add_settings_metabox(){
+    public static function add_settings_metabox(){
         add_meta_box( "salt_shaker_settings_metabox", __("Salt Changing Behaviour", SALT_SHAKER_DOMAIN), array(__CLASS__, "metabox_content") , "saltshaker", "normal" );
     }
     
-    public function admin_page_content(){
+    public static function admin_page_content(){
         include_once(plugin_dir_path(__FILE__) . "gui/settings-template.php");
     }
     
-    public function metabox_content(){
+    public static function metabox_content(){
         include_once(plugin_dir_path(__FILE__) . "gui/inner-settings-template.php");
     }
     
-    public function enqueue_admin_scripts(){
+    public static function enqueue_admin_scripts(){
         wp_enqueue_script( 'salt_shaker_admin', plugin_dir_url( __FILE__ ) . 'gui/js/salt_shaker_admin.js', array("jquery") );
     }
     
