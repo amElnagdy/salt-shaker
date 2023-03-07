5917c6d6f9d24b9bba34b801cc7b32dc0d13b3eb
Ahmed <amElnagdy@users.noreply.github.com>
2023-03-07 20:33:10 +0200

    Adding an upgrade link in the settings page

--- _inc/gui/settings-template.php
+++ _inc/gui/settings-template.php
@@ -10,32 +10,47 @@
 			</div>
 		</div>
 	</form>
-	<div>
-		<?php
-			$url  = 'https://wordpress.org/support/plugin/salt-shaker/reviews/?rate=5#new-post';
-			$link = sprintf( wp_kses( __( 'Do you find this plugin useful? Please <a href="%s">Rate it</a> on WordPress.org. BIG Thanks in advance!', 'salt-shaker' ),
-				array(
-					'a' => array(
-						'href' => array()
+</div>
+<?php
+	if ( ss_fs()->is_not_paying() ) { ?>
+		<div style="margin-top: 20px">
+			<?php
+				$url  = 'https://www.buymeacoffee.com/nagdy';
+				$link = sprintf( wp_kses( __( 'Want to buy me a coffee? You <a href="%s">can do it from here.</a>', 'salt-shaker' ),
+					array(
+						'a' => array(
+							'href' => array()
+						)
 					)
-				)
-			),
-				esc_url( $url ) );
-			echo $link;
-		?>
+				),
+					esc_url( $url ) );
+				echo $link;
+			?>
+		</div>
+<section style="background-color: #f8f8f8; border: 1px solid #ccc; padding: 20px; border-radius: 5px; margin: 50px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+	<h2 style="text-align: center; font-size: 24px; color: #333; text-shadow: 1px 1px #fff;"><?php esc_html_e( 'Looking for PRO Features?', 'salt-shaker' ); ?></h2>
+	<ul style="list-style-type: circle; margin: 10px; padding: 10px; font-size: 16px;">
+		<li style="margin-bottom: 10px;"><?php esc_html_e( 'Set exactly what time to change the salt keys: You can choose the time when your users are sleeping!.', 'salt-shaker' ); ?></li>
+		<li style="margin-bottom: 10px;"><?php esc_html_e( 'Get notified when the keys are changed: Receive an email notification when the salt keys are updated.', 'salt-shaker' ); ?></li>
+		<li style="margin-bottom: 10px;"><?php esc_html_e( 'Set a custom email for receiving the notifications: Choose the email address where you want to receive the notification emails.', 'salt-shaker' ); ?></li>
+		<li style="margin-bottom: 0;"><?php esc_html_e( 'Keep users logged in after the keys are changed: Enable this feature to prevent users from getting logged out after the salt keys are updated.', 'salt-shaker' ); ?></li>
+	</ul>
+	<div style="text-align: center; margin-top: 20px;">
+		<a href="<?php echo esc_url( ss_fs()->get_upgrade_url() ); ?>" style="display: inline-block; padding: 10px 20px; background-color: #333; color: #fff; border-radius: 5px; text-decoration: none; font-size: 18px; text-shadow: 1px 1px #000;"><?php esc_html_e( 'Upgrade to PRO', 'salt-shaker' ); ?></a>
 	</div>
-	<div style="margin-top: 20px">
-		<?php
-			$url  = 'https://www.buymeacoffee.com/nagdy';
-			$link = sprintf( wp_kses( __( 'Want to buy me a coffee? You <a href="%s">can do it from here.</a>', 'salt-shaker' ),
-				array(
-					'a' => array(
-						'href' => array()
-					)
+</section>
+<?php } ?>
+<div>
+	<?php
+		$url  = 'https://wordpress.org/support/plugin/salt-shaker/reviews/?rate=5#new-post';
+		$link = sprintf( wp_kses( __( 'Do you find this plugin useful? Please <a href="%s">Rate it</a> on WordPress.org. BIG Thanks in advance!', 'salt-shaker' ),
+			array(
+				'a' => array(
+					'href' => array()
 				)
-			),
-				esc_url( $url ) );
-			echo $link;
-		?>
-	</div>
+			)
+		),
+			esc_url( $url ) );
+		echo $link;
+	?>
 </div>
